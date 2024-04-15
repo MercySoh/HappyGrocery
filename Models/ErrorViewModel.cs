f91ed05703e4dab3cdeef20c5ecdcc03c8826556
Mercy <d00251752@student.dkit.ie>
2024-04-15 14:59:56 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HappyGrocery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
