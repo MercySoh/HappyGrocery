ce71ed361b33318c0e6c630cde05164b49217e2d
Mercy <d00251752@student.dkit.ie>
2024-04-15 15:17:44 +0100

    Add product model class

---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HappyGrocery.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string? ProductName { get; set; }
+
+        [Required] 
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public int Qty { get; set; }
+
+        public int CategoryId {  get; set; }
+    }
+}
