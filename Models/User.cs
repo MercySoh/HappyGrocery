fe916526ab4665173978ef45df2d0965eb789893
Mercy <d00251752@student.dkit.ie>
2024-04-15 15:39:07 +0100

    Add User model class

---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HappyGrocery.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? Firstname { get; set; }
+
+        [Required]
+        public string? Lastname { get; set;}
+
+        public int Usertype { get; set; }
+    }
+}
