ebb74f2229789f34b44adad403f5aafa414ac3c5
Mercy <d00251752@student.dkit.ie>
2024-04-17 11:30:07 +0100

    Add ProductDto model

---
+++ Models/ProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HappyGrocery.Models
+{
+    public class ProductDto
+    {
+        [Required]
+        public string? ProductName { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
