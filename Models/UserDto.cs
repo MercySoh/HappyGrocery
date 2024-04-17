50402bcd3d0e091ef580d32d3cf65fe15aee1169
Mercy <d00251752@student.dkit.ie>
2024-04-17 11:32:06 +0100

    Add UserDto model

---
+++ Models/UserDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HappyGrocery.Models
+{
+    public class UserDto
+    {
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? Firstname { get; set; }
+
+        [Required]
+        public string? Lastname { get; set; }
+    }
+}
